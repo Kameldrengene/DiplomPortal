17f95f2b7fe11d4653aeb666d8a8c3bdb96160d0
Talha-Butt786 <s195475@student.dtu.dk>
2021-09-27 19:36:18 +0200

    Nav bar added

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -8,13 +8,13 @@ class NavigationBar extends React.Component{
     render() {
         return(
             <Navbar collapseOnSelect expand="lg" bg = "light" variant="light" >
-                <Container  >
+                <Container>
                     <Navbar.Brand href="/" >
                         <img src="https://i2.wp.com/mlsm.man.dtu.dk/wp-content/uploads/2018/10/technical-university-of-denmark-dtu-19-logo.png?fit=733%2C238&ssl=1"
-                         width="200" height="70"/>
+                             width="200" height="70"/>
                     </Navbar.Brand>
                     <Navbar.Collapse id="responsive-navbar-nav" >
-                        <Nav className="gap-6">
+                        <Nav className="gap-6 me-auto">
                             <Nav.Link href="/">Kurser</Nav.Link>
                             <Nav.Link href="/kalender">Kalender</Nav.Link>
                             <Nav.Link href="/karakter">Karakter</Nav.Link>
