b5c7870be7ab2948d6c92d951cda5ac71cefb2fd
Frosty-laptop <bluedogdk@gmail.com>
2021-09-28 14:38:18 +0200

    Kursus side med emner

--- src/Components/Kursus.js
+++ src/Components/Kursus.js
@@ -1,13 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {useParams} from "react-router-dom";
+import {Grid, Paper, styled} from "@material-ui/core";
+import {observer} from "mobx-react";
+import {courseStore} from "../Stores/CourseStore";
+
 
 class kursus extends React.Component {
-
     render() {
-
+        var id = this.props.match.params.id
         return <div>
-            <h1>Mit Kursus</h1>
+            <h1>Course {id}
+            </h1>
+            <Grid container spacing={2}>
+                {courseStore.courses.filter(course => course.id === id).map((course, key) =>
+                    <ul>
+                        {course.Lektioner.map((course, key) =>
+
+                        <li>{course.Dato} - {course.Beskrivelse} - {course.Lektier} - {course.Øvelser}</li>
+                    )}
+                    </ul>
+                )}
+
+            </Grid>
         </div>
     }
 }
