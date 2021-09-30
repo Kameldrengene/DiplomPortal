e3c095e7fb5937649566adb326b51a828dd73992
Talha-Butt786 <s195475@student.dtu.dk>
2021-09-30 23:49:45 +0200

    toggle added to navbar. profile needs to be removed

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,20 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import './NavigationBar.css'
 
 
 class NavigationBar extends React.Component{
     render() {
         return(
-            <Navbar collapseOnSelect expand="lg" bg = "light" variant="light" >
+            <Navbar collapseOnSelect expand="lg" bg = "light" variant="light" onToggle={(expanded) => {
+                if(expanded)
+                    document.getElementById("mynavs").classList.replace("gap-6", "gap-1");
+                else
+                    document.getElementById("mynavs").classList.replace("gap-1", "gap-6");
+            }}>
                 <Container>
                     <Navbar.Brand href="/" >
-                        <img src="https://i2.wp.com/mlsm.man.dtu.dk/wp-content/uploads/2018/10/technical-university-of-denmark-dtu-19-logo.png?fit=733%2C238&ssl=1"
+                        <img alt="DTU logo" src="https://i2.wp.com/mlsm.man.dtu.dk/wp-content/uploads/2018/10/technical-university-of-denmark-dtu-19-logo.png?fit=733%2C238&ssl=1"
                              width="200" height="70"/>
                     </Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav" >
-                        <Nav className="gap-6 me-auto">
+                    <Navbar.Toggle />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="gap-6" id="mynavs">
                             <Nav.Link href="/">Kurser</Nav.Link>
                             <Nav.Link href="/kalender">Kalender</Nav.Link>
                             <Nav.Link href="/karakter">Karakter</Nav.Link>
@@ -22,7 +27,7 @@ class NavigationBar extends React.Component{
                     </Navbar.Collapse>
                     <div>
                         <Navbar.Collapse className="gap-3">
-                            <img src="https://cdn.lifehack.org/wp-content/uploads/2014/03/shutterstock_97566446.jpg" className="rounded-circle" width="50" height="50"/>
+                            <img alt="profile picture" src="https://cdn.lifehack.org/wp-content/uploads/2014/03/shutterstock_97566446.jpg" className="rounded-circle" width="50" height="50"/>
                             <div className="d-grid">
                                 <text className="" >Maria</text>
                                 <text className="">s123456@student.dtu.dk</text>
